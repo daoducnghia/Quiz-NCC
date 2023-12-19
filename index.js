20722408c167ff3fdcc6a1616662d413df2d8640
nghiaxd <nghiadd.work@gmail.com>
2023-12-19 23:27:23 +0700

    initial code

---
+++ index.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("menu-item").classList.remove("hidden");
+}
+
+function closeNav() {
+  document.getElementById("menu-item").classList.add("hidden");
+  document.getElementById("button-nav").classList.remove("hidden");
+}
