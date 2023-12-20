3f44ad0f8161c4b22879269f5b4089462303b841
nghiaxd <nghiadd.work@gmail.com>
2023-12-20 09:12:05 +0700

    add FE desktop

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ function closeNav() {
   document.getElementById("menu-item").classList.add("hidden");
   document.getElementById("button-nav").classList.remove("hidden");
 }
+
+var w = window.innerWidth;
+if (w > 480) {
+  document.getElementById("button-nav").classList.add("hidden");
+  document.getElementById("menu-item").classList.remove("hidden");
+  document.getElementById("item-close").classList.add("hidden");
+}
